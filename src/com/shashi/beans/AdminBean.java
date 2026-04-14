73cab9d09efa72684404402dc9d558c12a0b40c6
Harsh Maurya <harsh060305@gmail.com>
2026-04-14 16:04:48 +0530

    Update AdminBean.java

--- src/com/shashi/beans/AdminBean.java
+++ src/com/shashi/beans/AdminBean.java
@@ -4,7 +4,7 @@ import java.io.Serializable;
 
 public class AdminBean implements Serializable {
 
-	
+	 
 	private static final long serialVersionUID = 1L;
 	private String fName;
 	private String lName;
